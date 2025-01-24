311f9ac7715a37bd424e34ffc50a59c3b7c77341
Juan Sant'anna <juangsantanna@gmail.com>
2025-01-23 22:50:55 -0300

    fix names in pet struct

--- model/pet.go
+++ model/pet.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Pet struct {
 	gorm.Model
-	name    string `json:"name" gorm:"size:100;not null`
-	age     int    `json:"age" gorm: "not null"`
-	species string `json:"species" gorm: "size:50";not null`
-	owner   string `json:"owner" gorm:"size"`
+	Name    string `json:"Name" gorm:"size:100;not null`
+	Age     int    `json:"Age" gorm: "not null"`
+	Species string `json:"Species" gorm: "size:50";not null`
+	Owner   string `json:"Owner" gorm:"size"`
 }
