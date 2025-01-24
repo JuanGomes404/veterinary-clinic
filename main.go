4b12b86edf932dd88e5732899ca9fe2e5c7e4357
Juan Sant'anna <juangsantanna@gmail.com>
2025-01-23 23:25:10 -0300

    add listener and other settings

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"VeterinaryClinic/config"
+	"VeterinaryClinic/routes"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	config.ConnectDatabase()
+
+	router := mux.NewRouter()
+
+	routes.RegisterPetRoutes(router)
+
+	log.Println("Server running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
