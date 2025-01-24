7d96c914272110dbd648dca6028250cd32d3ac6e
Juan Sant'anna <juangsantanna@gmail.com>
2025-01-23 22:53:19 -0300

    fix syntax

--- model/pet.go
+++ model/pet.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Pet struct {
 	gorm.Model
-	Name    string `json:"Name" gorm:"size:100;not null`
-	Age     int    `json:"Age" gorm: "not null"`
-	Species string `json:"Species" gorm: "size:50";not null`
+	Name    string `json:"Name" gorm:"size:100;not null"`
+	Age     int    `json:"Age" gorm:"not null"`
+	Species string `json:"Species" gorm:"size:50;not null"`
 	Owner   string `json:"Owner" gorm:"size"`
 }
