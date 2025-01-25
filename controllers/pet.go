f19d1605c9736657ef1472ce2f69c4eba70ae800
Juan Sant'anna <juangsantanna@gmail.com>
2025-01-25 02:07:22 -0300

    add func to return errors with json

--- controllers/pet.go
+++ controllers/pet.go
@@ -10,11 +10,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func writeJSONError(w http.ResponseWriter, statusCode int, message string) {
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	errResponse := map[string]string{"error": message}
+	json.NewEncoder(w).Encode(errResponse)
+
+}
 func GetPets(w http.ResponseWriter, r *http.Request) {
 	pets, err := services.GetAllPets()
-	w.Header().Set("Content-Type", "application/json")
 	if err != nil {
-		http.Error(w, "Erro ao buscar os pets", http.StatusInternalServerError)
+		http.Error(w, "Error fetching pets", http.StatusInternalServerError)
 		return
 	}
 	json.NewEncoder(w).Encode(pets)
@@ -25,16 +32,15 @@ func GetPetById(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	id, err := strconv.Atoi(params["id"])
 	if err != nil {
-		http.Error(w, "ID inválido", http.StatusBadRequest)
+		writeJSONError(w, http.StatusInternalServerError, "Error fetching pets")
 		return
 	}
 
 	pet, err := services.GetPetByID(uint(id))
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
+		writeJSONError(w, http.StatusNotFound, err.Error())
 		return
 	}
-
 	json.NewEncoder(w).Encode(pet)
 }
 
@@ -42,13 +48,13 @@ func CreatePet(w http.ResponseWriter, r *http.Request) {
 	var pet model.Pet
 	err := json.NewDecoder(r.Body).Decode(&pet)
 	if err != nil {
-		http.Error(w, "Dados inválidos", http.StatusBadRequest)
+		writeJSONError(w, http.StatusBadRequest, "Invalid data")
 		return
 	}
 
 	err = services.CreatePet(&pet)
 	if err != nil {
-		http.Error(w, "Erro ao criar o pet", http.StatusInternalServerError)
+		writeJSONError(w, http.StatusInternalServerError, "Error registering the pet")
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
@@ -60,22 +66,22 @@ func UpdatePet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(params["id"])
 	if err != nil {
 		http.Error(w, "ID inválido", http.StatusBadRequest)
+		writeJSONError(w, http.StatusBadRequest, "Invalid id")
 		return
 	}
 
 	var updatedPet model.Pet
 	err = json.NewDecoder(r.Body).Decode(&updatedPet)
 	if err != nil {
-		http.Error(w, "Dados inválidos", http.StatusBadRequest)
+		writeJSONError(w, http.StatusBadRequest, "Invalid data")
 		return
 	}
 
 	err = services.UpdatePet(uint(id), &updatedPet)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(updatedPet)
 }
@@ -84,15 +90,15 @@ func DeletePet(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id, err := strconv.Atoi(params["id"])
 	if err != nil {
-		http.Error(w, "ID inválido", http.StatusBadRequest)
+		writeJSONError(w, http.StatusBadRequest, "Invalid id")
 		return
 	}
 
 	err = services.DeletePet(uint(id))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
+		writeJSONError(w, http.StatusNotFound, err.Error())
 		return
 	}
-
 	w.WriteHeader(http.StatusNoContent)
 }
