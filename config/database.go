7e51c733eeefb465184dc23af50c1fd4a2a318d4
Juan Sant'anna <juangsantanna@gmail.com>
2025-01-26 17:25:09 -0300

    create function retriving secret from gcp

--- config/database.go
+++ config/database.go
@@ -1,6 +1,12 @@
 package config
 
 import (
+	"context"
+	"fmt"
+	"log"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 	"gorm.io/gorm"
 )
 
@@ -10,6 +16,25 @@ func ConnectDatabase() *gorm.DB {
 
 }
 
-func GetSecrets(version string) {
+func GetSecret(projectID, secretName string) (string, error) {
+	ctx := context.Background()
 
+	c, err := secretmanager.NewClient(ctx)
+
+	if err != nil {
+		log.Fatalf("Fail to create a client from Secret Manager: &v", err)
+	}
+	defer c.Close()
+
+	fullSecretName := fmt.Sprintf("projects/%s/secrets/%s/versions/latest", projectID, secretName)
+
+	request := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: fullSecretName,
+	}
+	result, err := c.AccessSecretVersion(ctx, request)
+
+	if err != nil {
+		log.Fatalf("Cannot get the secret: %v", err)
+	}
+	return string(result.Payload.Data), err
 }
